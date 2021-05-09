82f8b2df937c70e268c8b41cb014aa0027552e98
neuuneu <101702051@student.swin.edu.au>
2021-05-09 16:51:12 +1000

    Added update user stpred procedure.

---
+++ TCABS_DB/dbo/Stored Procedures/spUpdateUser.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spUpdateUser]
+   @Username NVARCHAR(50),
+   @FirstName NVARCHAR(50),
+   @LastName NVARCHAR(100),
+   @Email NVARCHAR(100),
+   @PhoneNo INT,
+   @Password NVARCHAR(50)
+AS
+BEGIN
+   SET NOCOUNT ON;
+
+   UPDATE [dbo].[User] (Username, FirstName, LastName, Email, PhoneNo, [Password] )
+   VALUES ( @Username, @FirstName, @LastName, @Email, @PhoneNo, @Password )
+END;
