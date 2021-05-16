dc5ed029e9e291d628bf859fb84d8858cd54ca1d
neuuneu <101702051@student.swin.edu.au>
2021-05-16 16:52:08 +1000

    Fixed spUpdateUser

--- TCABS_DB/dbo/Stored Procedures/spUpdateUser.sql
+++ TCABS_DB/dbo/Stored Procedures/spUpdateUser.sql
@@ -1,14 +1,24 @@
 ﻿CREATE PROCEDURE [dbo].[spUpdateUser]
+   @UserId INT,
    @Username NVARCHAR(50),
    @FirstName NVARCHAR(50),
    @LastName NVARCHAR(100),
    @Email NVARCHAR(100),
    @PhoneNo INT,
-   @Password NVARCHAR(50)
+   @Password NVARCHAR(64),
+   @PasswordSalt NVARCHAR(64)
 AS
 BEGIN
    SET NOCOUNT ON;
 
-   UPDATE [dbo].[User] (Username, FirstName, LastName, Email, PhoneNo, [Password] )
-   VALUES ( @Username, @FirstName, @LastName, @Email, @PhoneNo, @Password )
+	UPDATE [dbo].[User]
+	SET 
+		Username = @Username,
+		FirstName = @FirstName,
+		LastName = @LastName,
+		Email = @Email,
+		PhoneNo = @PhoneNo,
+		[Password] = @Password,
+		PasswordSalt = @PasswordSalt
+	WHERE [UserId] = @UserId;
 END;
