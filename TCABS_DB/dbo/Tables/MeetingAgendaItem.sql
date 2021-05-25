ca167e40f69a626743dbc0bd7f1659102d0ac470
Pestery <37169741+Pestery@users.noreply.github.com>
2021-05-25 15:56:10 +1000

    Added database tables which Noor and Neusha worked on.

---
+++ TCABS_DB/dbo/Tables/MeetingAgendaItem.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[MeetingAgendaItem]
+(
+	[MeetingAgendaItemId] INT IDENTITY(1,1) NOT NULL,
+	[MeetingId] INT NOT NULL,
+	[EnrollmentId] INT NOT NULL,
+	[Comment]  NVARCHAR(50) NOT NULL,
+	CONSTRAINT PK_MeetingAgendaItemId PRIMARY KEY (MeetingAgendaItemId),
+	CONSTRAINT FK_MeetingAgendaItem_MeetingId FOREIGN KEY (MeetingId) REFERENCES [Meeting] (MeetingId),
+	CONSTRAINT FK_MeetingAgendaItem_EnrollementId FOREIGN KEY (EnrollmentId) REFERENCES [Enrollment] (EnrollmentId)
+)
---
+++ TCABS_DB/dbo/Tables/Task.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Task]
+(
+	[TaskId] INT IDENTITY(1,1) NOT NULL,
+	[Name] NVARCHAR(50) NOT NULL,
+	[ProjectRoleId] INT NOT NULL,
+	CONSTRAINT PK_TaskId PRIMARY KEY (TaskId),
+	CONSTRAINT FK_Task_ProjectRoleId FOREIGN KEY (ProjectRoleId) REFERENCES [ProjectRole] (ProjectRoleId)
+)
