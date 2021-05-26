b98becc63bcdc13e1636aefdb15133401c8c934b
neuuneu <101702051@student.swin.edu.au>
2021-05-26 18:51:30 +1000

    Updated delete user

--- TCABS_DB/dbo/Stored Procedures/spDeleteUser.sql
+++ TCABS_DB/dbo/Stored Procedures/spDeleteUser.sql
@@ -2,6 +2,27 @@
    @userid int
 AS
 BEGIN
-   DELETE [User] 
-   WHERE UserId = @userid
+
+	DECLARE @enrollmentCount INT;
+	DECLARE @UnitofferingCount INT;
+	DECLARE @TeamCount INT;
+
+	SELECT @enrollmentCount = COUNT(*) FROM [dbo].[Enrollment] WHERE UserId = @userid;
+	SELECT @unitofferingCount = COUNT(*) FROM [dbo].[UnitOffering] WHERE UserId = @userid;
+	SELECT @TeamCount = COUNT(*) FROM [dbo].[Team] WHERE SupervisorId = @userid;
+
+	IF @enrollmentCount = 0 AND @unitofferingCount = 0
+	BEGIN
+
+		DELETE FROM [dbo].[UserRole]
+	   WHERE UserId = @userid;
+
+	   DELETE FROM [dbo].[User] 
+	   WHERE UserId = @userid;
+	END
+	ELSE
+	BEGIN
+		RAISERROR('Unable to delete user', 16, 1);
+	END
+
 END;
