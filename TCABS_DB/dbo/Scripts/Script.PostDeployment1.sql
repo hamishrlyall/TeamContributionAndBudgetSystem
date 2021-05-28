412e60e1e2286ed8feaf698df129ac05a02893f6
Hamish Lyall <hamishrlyall@gmail.com>
2021-05-28 17:18:23 +1000

    Merged Matt's stuff into master, and changed pre/post deployment scripts

--- TCABS_DB/dbo/Scripts/Script.PostDeployment1.sql
+++ TCABS_DB/dbo/Scripts/Script.PostDeployment1.sql
@@ -14,11 +14,11 @@ BEGIN
 -- Create test users
 MERGE INTO [User] AS Target USING ( 
    VALUES
-   (1, 'superadmin', 'super', 'user', 'superadmin@email.com', 0400000000, 'password'),
-   (2, 'admin','admin', 'user', 'admin@email.com', 0400000000, 'password'),
-   (3, 'convenor','convenor', 'user', 'convener@email.com', 0400000000, 'password'),
-   (4, 'student','student', 'user', 'student@email.com', 0400000000, 'password'),
-   (5, 'supervisor','supervisor', 'user', 'supervisor@email.com', 0400000000, 'password')
+   (1, 'superadmin', 'super', 'user', 'superadmin@email.com', 0400000000, 'password')
+   --(2, 'admin','admin', 'user', 'admin@email.com', 0400000000, 'password'),
+   --(3, 'convenor','convenor', 'user', 'convener@email.com', 0400000000, 'password'),
+   --(4, 'student','student', 'user', 'student@email.com', 0400000000, 'password'),
+   --(5, 'supervisor','supervisor', 'user', 'supervisor@email.com', 0400000000, 'password')
 )
 AS Source ([UserId], [UserName], [FirstName], [LastName], [Email], [PhoneNo], [Password]) 
 ON Target.UserId = Source.UserId
@@ -69,11 +69,11 @@ VALUES ([PermissionName], [LinkGroup], [LinkTitle], [LinkPage], [LinkController]
 -- Link Users to roles
 MERGE INTO [UserRole] AS Target USING (
  VALUES
- (1, 1, 1),
- (2, 2, 2),
- (3, 3, 3),
- (4, 4, 4),
- (5, 5, 5)
+ (1, 1, 1)
+ --(2, 2, 2),
+ --(3, 3, 3),
+ --(4, 4, 4),
+ --(5, 5, 5)
  )
  AS Source ([UserRoleId], [UserId], [RoleId])
  ON Target.UserRoleId = Source.UserRoleId
@@ -84,29 +84,29 @@ MERGE INTO [UserRole] AS Target USING (
 -- Link permissions to roles
 MERGE INTO [RolePermission] AS Target USING (
    VALUES
-   (1, 1, 1),
-   (2, 1, 2),
-   (3, 1, 3),
-   (4, 1, 4),
-   (5, 1, 5),
-   (6, 1, 6),
-   (7, 1, 7),
-   (8, 1, 8),
-   (9, 1, 9),
-   (10, 1, 10),
-   (11, 1, 11),
-   (12, 1, 12),
-   (13, 2, 2),
-   (14, 2, 3),
-   (15, 2, 4),
-   (16, 2, 5),
-   (17, 2, 6),
-   (18, 2, 7),
-   (19, 2, 8),
-   (20, 2, 9),
-   (21, 2, 10),
-   (22, 2, 11),
-   (23, 2, 12)
+   (1, 1, 1)
+   --(2, 1, 2),
+   --(3, 1, 3),
+   --(4, 1, 4),
+   --(5, 1, 5),
+   --(6, 1, 6),
+   --(7, 1, 7),
+   --(8, 1, 8),
+   --(9, 1, 9),
+   --(10, 1, 10),
+   --(11, 1, 11),
+   --(12, 1, 12),
+   --(13, 2, 2),
+   --(14, 2, 3),
+   --(15, 2, 4),
+   --(16, 2, 5),
+   --(17, 2, 6),
+   --(18, 2, 7),
+   --(19, 2, 8),
+   --(20, 2, 9),
+   --(21, 2, 10),
+   --(22, 2, 11),
+   --(23, 2, 12)
 )
 AS Source ([RolePermissionId], [RoleId], [PermissionId])
 ON Target.RolePermissionId = Source.RolePermissionId
@@ -114,109 +114,109 @@ WHEN NOT MATCHED BY TARGET THEN
 INSERT ( [RoleId], [PermissionId])
 VALUES ( [RoleId], [PermissionId]);
 
-MERGE INTO [Unit] AS Target USING (
-   VALUES
-   (1, 'INF300111' ),
-   (2, 'SWE30010' ),
-   (3, 'COS20001' ),
-   (4, 'SWD5002' )
-)
-AS Source ([UnitId], [Name])
-ON Target.UnitId = Source.UnitId
-WHEN NOT MATCHED BY TARGET THEN
-INSERT ( [Name] )
-VALUES ( [Name] );
+--MERGE INTO [Unit] AS Target USING (
+--   VALUES
+--   (1, 'INF300111' ),
+--   (2, 'SWE30010' ),
+--   (3, 'COS20001' ),
+--   (4, 'SWD5002' )
+--)
+--AS Source ([UnitId], [Name])
+--ON Target.UnitId = Source.UnitId
+--WHEN NOT MATCHED BY TARGET THEN
+--INSERT ( [Name] )
+--VALUES ( [Name] );
 
-MERGE INTO [TeachingPeriod] AS Target USING(
-   VALUES
-   ( 1, 'Summer', 1, 4 ),
-   ( 2, 'Semester 1', 3, 1 ),
-   ( 3, 'Winter', 6, 21 ),
-   ( 4, 'Semester 2', 8, 2 )
-)
-AS Source ([TeachingPeriodId], [Name], [Month], [Day] )
-ON Target.TeachingPeriodId = Source.TeachingPeriodId
-WHEN NOT MATCHED BY TARGET THEN
-INSERT ( [Name], [Month], [Day] )
-VALUES ( [Name], [Month], [Day] );
+--MERGE INTO [TeachingPeriod] AS Target USING(
+--   VALUES
+--   ( 1, 'Summer', 1, 4 ),
+--   ( 2, 'Semester 1', 3, 1 ),
+--   ( 3, 'Winter', 6, 21 ),
+--   ( 4, 'Semester 2', 8, 2 )
+--)
+--AS Source ([TeachingPeriodId], [Name], [Month], [Day] )
+--ON Target.TeachingPeriodId = Source.TeachingPeriodId
+--WHEN NOT MATCHED BY TARGET THEN
+--INSERT ( [Name], [Month], [Day] )
+--VALUES ( [Name], [Month], [Day] );
 
-MERGE INTO [Year] AS Target USING(
-   VALUES
-   ( 1, 2020 ),
-   ( 2, 2021 ),
-   ( 3, 2022 ),
-   ( 4, 2023 ),
-   ( 5, 2024 )
-)
-AS Source ([YearId], [Year])
-ON Target.YearId = Source.YearId
-WHEN NOT MATCHED BY TARGET THEN
-INSERT ( [Year] )
-VALUES ( [Year] );
+--MERGE INTO [Year] AS Target USING(
+--   VALUES
+--   ( 1, 2020 ),
+--   ( 2, 2021 ),
+--   ( 3, 2022 ),
+--   ( 4, 2023 ),
+--   ( 5, 2024 )
+--)
+--AS Source ([YearId], [Year])
+--ON Target.YearId = Source.YearId
+--WHEN NOT MATCHED BY TARGET THEN
+--INSERT ( [Year] )
+--VALUES ( [Year] );
 
-MERGE INTO [UnitOffering] AS Target USING(
-   VALUES
-   ( 1, 3, 1, 2, 1 ),
-   ( 2, 3, 2, 2, 1 ),
-   ( 3, 3, 3, 2, 1 ),
-   ( 4, 3, 4, 2, 1 )
-)
-AS Source ([UnitOfferingId], [ConvenorId], [UnitId], [TeachingPeriodId], [YearId] )
-ON TARGET.UnitOfferingId = Source.UnitOfferingId
-WHEN NOT MATCHED BY TARGET THEN
-INSERT ( [ConvenorId], [UnitId], [TeachingPeriodId], [YearId] )
-VALUES ( [ConvenorId], [UnitId], [TeachingPeriodId], [YearId] );
+--MERGE INTO [UnitOffering] AS Target USING(
+--   VALUES
+--   ( 1, 3, 1, 2, 1 ),
+--   ( 2, 3, 2, 2, 1 ),
+--   ( 3, 3, 3, 2, 1 ),
+--   ( 4, 3, 4, 2, 1 )
+--)
+--AS Source ([UnitOfferingId], [ConvenorId], [UnitId], [TeachingPeriodId], [YearId] )
+--ON TARGET.UnitOfferingId = Source.UnitOfferingId
+--WHEN NOT MATCHED BY TARGET THEN
+--INSERT ( [ConvenorId], [UnitId], [TeachingPeriodId], [YearId] )
+--VALUES ( [ConvenorId], [UnitId], [TeachingPeriodId], [YearId] );
 
--- Create project roles
-MERGE INTO [ProjectRole] AS Target USING (
-   VALUES
-   (1, 'Team Leader'),
-   (2, 'Programmer')
-)
-AS Source ([ProjectRoleId], [Name])
-ON Target.[ProjectRoleId] = Source.[ProjectRoleId]
-WHEN NOT MATCHED BY TARGET THEN
-INSERT ([Name])
-VALUES ([Name]);
-
-
--- Create project role groups
-MERGE INTO [ProjectRoleGroup] AS Target USING (
-   VALUES
-   (1, 'RoleGroup1'),
-   (2, 'RoleGroup2')
-)
-AS Source ([ProjectRoleGroupId], [Name])
-ON Target.ProjectRoleGroupId = Source.ProjectRoleGroupId
-WHEN NOT MATCHED BY TARGET THEN
-INSERT ([Name])
-VALUES ([Name]);
+---- Create project roles
+--MERGE INTO [ProjectRole] AS Target USING (
+--   VALUES
+--   (1, 'Team Leader'),
+--   (2, 'Programmer')
+--)
+--AS Source ([ProjectRoleId], [Name])
+--ON Target.[ProjectRoleId] = Source.[ProjectRoleId]
+--WHEN NOT MATCHED BY TARGET THEN
+--INSERT ([Name])
+--VALUES ([Name]);
 
 
--- Create projects
-MERGE INTO [Project] AS Target USING (
-   VALUES
-   (1, 'Project1','Description for Project1', 1),
-   (2, 'Project2','Description for Project2', 1)
-)
-AS Source ([ProjectId], [Name], [Description], [ProjectRoleGroupId])
-ON Target.ProjectId = Source.ProjectRoleGroupId
-WHEN NOT MATCHED BY TARGET THEN
-INSERT ([Name], [Description], [ProjectRoleGroupId])
-VALUES ([Name], [Description], [ProjectRoleGroupId]);
+---- Create project role groups
+--MERGE INTO [ProjectRoleGroup] AS Target USING (
+--   VALUES
+--   (1, 'RoleGroup1'),
+--   (2, 'RoleGroup2')
+--)
+--AS Source ([ProjectRoleGroupId], [Name])
+--ON Target.ProjectRoleGroupId = Source.ProjectRoleGroupId
+--WHEN NOT MATCHED BY TARGET THEN
+--INSERT ([Name])
+--VALUES ([Name]);
 
 
--- Create project role linkss
-MERGE INTO [ProjectRoleLink] AS Target USING (
-   VALUES
-   (1, 1, 1),
-   (2, 2, 1)
-)
-AS Source ([ProjectRoleLinkId], [ProjectRoleId], [ProjectRoleGroupId])
-ON Target.ProjectRoleLinkId = Source.ProjectRoleLinkId
-WHEN NOT MATCHED BY TARGET THEN
-INSERT ([ProjectRoleId], [ProjectRoleGroupId])
-VALUES ([ProjectRoleId], [ProjectRoleGroupId]);
+---- Create projects
+--MERGE INTO [Project] AS Target USING (
+--   VALUES
+--   (1, 'Project1','Description for Project1', 1),
+--   (2, 'Project2','Description for Project2', 1)
+--)
+--AS Source ([ProjectId], [Name], [Description], [ProjectRoleGroupId])
+--ON Target.ProjectId = Source.ProjectRoleGroupId
+--WHEN NOT MATCHED BY TARGET THEN
+--INSERT ([Name], [Description], [ProjectRoleGroupId])
+--VALUES ([Name], [Description], [ProjectRoleGroupId]);
+
+
+---- Create project role linkss
+--MERGE INTO [ProjectRoleLink] AS Target USING (
+--   VALUES
+--   (1, 1, 1),
+--   (2, 2, 1)
+--)
+--AS Source ([ProjectRoleLinkId], [ProjectRoleId], [ProjectRoleGroupId])
+--ON Target.ProjectRoleLinkId = Source.ProjectRoleLinkId
+--WHEN NOT MATCHED BY TARGET THEN
+--INSERT ([ProjectRoleId], [ProjectRoleGroupId])
+--VALUES ([ProjectRoleId], [ProjectRoleGroupId]);
 
 
 
--- TCABS_DB/dbo/Scripts/Script.PreDeployment1.sql
+++ TCABS_DB/dbo/Scripts/Script.PreDeployment1.sql
@@ -9,30 +9,153 @@
                SELECT * FROM [$(TableName)]               
 --------------------------------------------------------------------------------------
 */
-/*
-DROP TABLE IF EXISTS [dbo].[RolePermission];
 
-DROP TABLE IF EXISTS [dbo].[Permission];
+--DROP TABLE IF EXISTS [dbo].[RolePermission];
 
-DROP TABLE IF EXISTS [dbo].[UserRole];
+--DROP TABLE IF EXISTS [dbo].[Permission];
 
-DROP TABLE IF EXISTS [dbo].[Role];
+--DROP TABLE IF EXISTS [dbo].[UserRole];
 
-DROP TABLE IF EXISTS [dbo].[User];
+--DROP TABLE IF EXISTS [dbo].[Role];
 
-DROP PROCEDURE IF EXISTS [dbo].[spCreateUser];
+--DROP TABLE IF EXISTS [dbo].[Minute]
 
-DROP PROCEDURE IF EXISTS [dbo].[spGetUserRoleByUserId];
+--DROP TABLE IF EXISTS [dbo].[MeetingAgendaItem]
 
-DROP PROCEDURE IF EXISTS [dbo].[spInsertUserRole];
+--DROP TABLE IF EXISTS [dbo].[MeetingAttendee]
 
-DROP PROCEDURE IF EXISTS [dbo].[spLoadUsers];
+--DROP TABLE IF EXISTS [dbo].[ProjectTask]
 
-DROP PROCEDURE IF EXISTS [dbo].[spSelectUserForUserId];
+--DROP TABLE IF EXISTS [dbo].[Enrollment]
 
-DROP PROCEDURE IF EXISTS [dbo].[spSelectUserWithRoles];
+--DROP TABLE IF EXISTS [dbo].[MeetingComment]
 
-DROP PROCEDURE IF EXISTS [dbo].[spLoadUsers];
+--DROP TABLE IF EXISTS [dbo].[Meeting]
 
-DROP PROCEDURE IF EXISTS [dbo].[spUpdatePassword]
---*/
+--DROP TABLE IF EXISTS [dbo].[ProjectOffering]
+
+--DROP TABLE IF EXISTS [dbo].[UnitOffering]
+
+--DROP TABLE IF EXISTS [dbo].[Unit]
+
+--DROP TABLE IF EXISTS [dbo].[Year]
+
+--DROP TABLE IF EXISTS [dbo].[TeachingPeriod]
+
+--DROP TABLE IF EXISTS [dbo].[Project]
+
+--DROP TABLE IF EXISTS [dbo].[ProjectRoleLink]
+
+--DROP TABLE IF EXISTS [dbo].[ProjectRoleGroup]
+
+--DROP TABLE IF EXISTS [dbo].[ProjectRole]
+
+--DROP TABLE IF EXISTS [dbo].[ProjectTask]
+
+--DROP TABLE IF EXISTS [dbo].[User]
+
+--DROP TABLE IF EXISTS [dbo].[Team]
+
+
+
+--DROP PROCEDURE IF EXISTS [dbo].[spApproveProjectTask]
+--DROP PROCEDURE IF EXISTS [dbo].[spCreateProject]
+--DROP PROCEDURE IF EXISTS [dbo].[spCreateProjectRole]
+--DROP PROCEDURE IF EXISTS [dbo].[spCreateProjectRoleGroup]
+--DROP PROCEDURE IF EXISTS [dbo].[spCreateProjectRoleLink]
+--DROP PROCEDURE IF EXISTS [dbo].[spCreateProjectTask]
+--DROP PROCEDURE IF EXISTS [dbo].[spCreateRolePermission]
+--DROP PROCEDURE IF EXISTS [dbo].[spCreateUser]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteEnrollment]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteProject]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteProjectOffering]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteProjectRole]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteProjectRoleGroup]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteProjectRoleLink]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteRolePermission]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteTeachingPeriod]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteTeam]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteUnit]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteUnitOffering]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteUser]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteUserRole]
+--DROP PROCEDURE IF EXISTS [dbo].[spDeleteYear]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetAllProjectRoleGroups]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetAllProjectRoles]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetAllProjects]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetAllProjectTasks]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetPermissionsFromUsername]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetProject]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetProjectForProjectOfferingId]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetProjectOfferingsForUserId]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetProjectOfferingsForUserIdOnlyStudents]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetProjectRole]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetProjectRoleGroup]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetProjectRoleLinksForGroup]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetProjectRoleLinksForRole]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetProjectRolesForEnrollment]
+--DROP PROCEDURE IF EXISTS [dbo].[spGetProjectTask]
+--DROP PROCEDURE IF EXISTS [dbo].[spInsertEnrollment]
+--DROP PROCEDURE IF EXISTS [dbo].[spInsertProjectOffering]
+--DROP PROCEDURE IF EXISTS [dbo].[spInsertTeachingPeriod]
+--DROP PROCEDURE IF EXISTS [dbo].[spInsertTeam]
+--DROP PROCEDURE IF EXISTS [dbo].[spInsertUnit]
+--DROP PROCEDURE IF EXISTS [dbo].[spInsertUnitOffering]
+--DROP PROCEDURE IF EXISTS [dbo].[spInsertUserRole]
+--DROP PROCEDURE IF EXISTS [dbo].[spInsertYear]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectConvenors]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectEnrollmentCountForTeamIdAndUserId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectEnrollmentCountForUnitOfferingIdAndUserId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectEnrollmentForEnrollmentId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectEnrollmentsForTeamId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectEnrollmentsForUnitOfferingId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectPermission]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectPermissions]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectProjectOfferingForProjectOfferingId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectProjectOfferings]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectProjectOfferingsForUnitOfferingId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectRoleForRoleId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectRolePermissionsForRoleId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectRoles]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectRoleWithPermissions]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectStudents]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectSupervisors]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectTeachingPeriodForName]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectTeachingPeriodForTeachingPeriodId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectTeachingPeriods]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectTeam]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectTeamsForUnitOfferingId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUnitForName]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUnitForUnitId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUnitOfferingCountForTeachingPeriod]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUnitOfferingCountForUnit]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUnitOfferingCountForYear]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUnitOfferingForDetails]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUnitOfferingForUnitOfferingId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUnitOfferings]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUnitOfferingWithEnrollments]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUnits]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUserForUserId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUserForUsername]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUserFromEnrollment]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUserRole]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUserRoleCountForUserIdAndRoleId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUserRoles]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUserRolesForUserId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUsers]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectUserWithRoles]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectYearForYearId]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectYearForYearValue]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectYears]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdateEnrollmentWithTeamId]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdatePassword]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdateProject]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdateProjectRole]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdateProjectRoleGroup]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdateProjectTask]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdateTeachingPeriod]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdateTeam]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdateUnit]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdateUnitOffering]
+--DROP PROCEDURE IF EXISTS [dbo].[spUpdateUser]
+--DROP PROCEDURE IF EXISTS [dbo].[spSelectTeamsForProjectOfferingId]
